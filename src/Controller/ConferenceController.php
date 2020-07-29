d718ea0229f77228256bb681b732c7508a232e91
FESLARD Margaux <margaux.feslard@gmail.com>
2020-07-29 15:34:31 +0200

    all

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,21 +3,32 @@
 namespace App\Controller;
 
 use App\Entity\Conference;
+use App\Entity\Comment;
+use App\Form\CommentFormType;
+use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     private $twig;
+    private $entityManager;
 
-public function __construct(Environment $twig)
+public function __construct(Environment $twig, EntityManagerInterface $entityManager, MessageBusInterface $bus)
 {
-    $this->twig = $twig;  
+    $this->twig = $twig;
+    $this->entityManager = $entityManager; 
+    $this->bus = $bus; 
+
 }
     /**
      * @Route("/", name="homepage")
@@ -32,8 +43,36 @@ public function __construct(Environment $twig)
     /**
      * @Route("/conference/{slug}", name="conference")
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository)
+    public function show(Request $request, Conference $conference, ConferenceRepository $conferenceRepository, CommentRepository $commentRepository, string $photoDir)
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setConference($conference);
+            if($photo = $form['photo']->getData()){
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                try{
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e){
+
+                }
+                $comment->setPhotoFilename($filename);
+            }
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+            $context= [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referrer'),
+                'permalink' => $request->getUri(),
+            ];
+           $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+            
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         return new Response($this->twig->render('conference/show.html.twig', [
@@ -42,6 +81,7 @@ public function __construct(Environment $twig)
             'comments' => $paginator,
             'previous' =>$offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form'=> $form->createView(),
         ]));
     }
 }
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CommentRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CommentRepository::class)
@@ -20,6 +21,7 @@ class Comment
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
     private $author;
 
@@ -30,6 +32,8 @@ class Comment
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Email
      */
     private $email;
 
@@ -48,6 +52,11 @@ class Comment
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $photoFilename;
+
+    /**
+     * @ORM\Column(type="string", length=255, options={"default":"submitted"})
+     */
+    private $state = 'submitted';
 
     public function __toString(): string
     {
@@ -135,6 +144,18 @@ class Comment
     public function setPhotoFilename(?string $photoFilename): self
     {
         $this->photoFilename = $photoFilename;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
 
         return $this;
     }
